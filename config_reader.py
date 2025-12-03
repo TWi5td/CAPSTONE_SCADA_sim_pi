4928ca07260b81dd84aa43f7eac49e010c112024
TWi5td <157996871+TWi5td@users.noreply.github.com>
2025-12-03 14:39:40 -0800

    Delete config_reader.py

--- config_reader.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open('/home/SCADA/scada_project/scada_config/config.json', 'r') as f:
-    config = json.load(f)
-print("Protocol:", config["protocol"])
-print("Data Points:", config["data_points"])
